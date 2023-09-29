2f3b2b648e777219e2a9de399ad097f362dcf7d9
ReinaldoJr <reinaldocsj@id.uff.br>
2023-09-29 13:24:07 -0300

    Inclusao de arquivos faltantes no commit anterior.

--- servidor.py
+++ servidor.py
@@ -1,6 +1,5 @@
-
 from flask import Flask,jsonify,request
-from calculaTfidf import ListaVideosRelacionados,ListaVideosBusca,ListaVideosRecomendados
+from calculaTfidf import ListaVideosRelacionados,ListaVideosBusca,ListaVideosRecomendados,ListaVideosHistoricos
 
 app = Flask(__name__)
 
@@ -24,6 +23,20 @@ def processa_trilha():
     resposta = ListaVideosRecomendados(listaFinal)
     return jsonify(resposta)
 
+@app.route('/processa_historico', methods=['GET'])
+def processa_historico():
+    lista = request.args.get('playlist')
+    lista2 = lista.replace("[", "")
+    lista3 = lista2.replace("]", "")
+    lista4 = lista3.replace("\"", "")
+    lista4 = lista4.replace(" ","")
+    listaFinal = lista4.split(",")
+    for i in listaFinal:
+        print(i)
+    print(listaFinal)
+    resposta = ListaVideosHistoricos(listaFinal)
+    return jsonify(resposta)
+
 @app.route('/processa_busca', methods=['GET'])
 def processa_busca():
     frase_busca = request.args.get('query')
@@ -35,4 +48,4 @@ def home():
     return "Hello, World!"    
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, host='10.0.0.24')
+    app.run(debug=True, port=5000, host='0.0.0.0')
